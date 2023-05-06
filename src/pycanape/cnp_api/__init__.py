05246cb1411f406aa47b3040e0efe4e6ca97a348
zariiii9003 <52598363+zariiii9003@users.noreply.github.com>
2023-05-06 03:19:50 +0200

    update linters, add __all__

--- src/pycanape/cnp_api/__init__.py
+++ src/pycanape/cnp_api/__init__.py
@@ -1,3 +1,9 @@
 # SPDX-FileCopyrightText: 2022-present Artur Drogunow <artur.drogunow@zf.com>
 #
 # SPDX-License-Identifier: MIT
+
+__all__ = [
+    "cnp_class",
+    "cnp_constants",
+    "cnp_prototype",
+]
