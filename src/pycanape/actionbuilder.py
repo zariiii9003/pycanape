5123b181a7801e89755f40ad62265e6e8b24172e
Timothy <tim.heath@halla.com>
2024-01-30 14:43:58 -0500

    Should be just about done.

--- src/pycanape/actionbuilder.py
+++ src/pycanape/actionbuilder.py
@@ -1,4 +1,7 @@
+from typing import Optional, Union, Callable
 import canape
+
+CanapeAction = Union[Callable[[canape], canape], 'ActionBuilder']
 
 
 class ActionBuilder:
@@ -12,17 +15,35 @@ class ActionBuilder:
     from the user is minimized.
     """
 
-    def __init__(self):
-        ...
+    close_canape: bool
+    project_path: str
+    modal_mode: bool
+
+    context: Optional[canape.CANape]
+
+    def __init__(self, _action: CanapeAction = lambda x: x):
+        self._action = _action
 
     def __enter__(self):
-        ...
+        self.context = canape.CANape(self.project_path, self.modal_mode)
+        # TODO: figure out using context directly as CANape object.
 
     def __exit__(self):
-        ...
+        self.context.exit(self.close_canape)
 
-    def __call__(self):
-        ...
+    def __call__(self, context: Optional[CanapeAction] = None):
+        """Using this wrapper will lock
+        the application only for the
+        duration of this function."""
+        if context is None:
+            context = canape.CANape(self.project_path, self.modal_mode)
+            self._action(context)
+            context.exit(self.close_canape)
+        else:
+            self._action(context)
+            return context
 
-    def also(self):
-        ...
+    def also(self, next_action: CanapeAction) -> 'ActionBuilder':
+        """This function allows the chaining of
+        multiple operations into the same lock window."""
+        return ActionBuilder(lambda x: next_action(self._action(x)))
